98d09b1cf78000a6ed9cbdf50281a4f9795144d7
ZionYou <peter832916@gmail.com>
2023-05-27 18:16:38 +0800

    add check permission function for test-token api and permission for navigation at pages

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,6 +1,8 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
 import { useState, useEffect } from 'react';
 import { getTodos, createTodo, patchTodo, deleteTodo } from '../api/todos';
+import { useNavigate } from 'react-router-dom';
+import { checkPermission } from '../api/auth';
 
 const TodoPage = () => {
   // 捕捉最新的資料狀態
@@ -8,6 +10,7 @@ const TodoPage = () => {
   //  儲存所有的todoItem
   const [todos, setTodos] = useState([]);
   const count = todos.length;
+  const navigate = useNavigate();
 
   // 監聽子層傳來的 onChange
   const handleChange = (value) => {
@@ -142,6 +145,21 @@ const TodoPage = () => {
     };
     getTodosAsync();
   }, []);
+
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+        navigate('/login');
+      }
+      const result = await checkPermission(authToken);
+      if (!result) {
+        navigate('/login');
+      }
+    };
+
+    checkTokenIsValid();
+  }, [navigate]);
 
   return (
     <div>
